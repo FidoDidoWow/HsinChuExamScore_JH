d49668cef9e67d7484573b78d28b19e246201aaa
Ed Lin <yinghua.lin@ischool.com.tw>
2021-05-20 15:45:15 +0800

    修正更新欄位未出現問題

--- HsinChuExamScore_JH/DAO/UDT_KCBSDermitComparison.cs
+++ HsinChuExamScore_JH/DAO/UDT_KCBSDermitComparison.cs
@@ -27,7 +27,11 @@ namespace HsinChuExamScore_JH.DAO
         public string Name { get; set; }
 
 
-
+        /// <summary>
+        /// 懲戒單號
+        /// </summary>
+        [Field(Field = "goldmedal_code", Indexed = false)]
+        public string Goldmedal_code { get; set; }
 
 
 
